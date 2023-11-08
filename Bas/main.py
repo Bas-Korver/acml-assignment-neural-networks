0d69e18765a873c41b5b0e0542fe284f6c27943b
Bas Körver <Bas_Korver@outlook.com>
2023-11-08 19:32:33 +0100

    Fixed small mistake in main.py
    Changed add_bias=False to add_bias=True

--- Bas/main.py
+++ Bas/main.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     training_examples = np.identity(8)
 
     number_generation_kwargs = {'mu': 0, 'sigma': 0.1}
-    ann = ann.ANN(8, [3], 8, sigmoid, squared_error, normal, zeros, add_bias=False, **number_generation_kwargs)
+    ann = ann.ANN(8, [3], 8, sigmoid, squared_error, normal, zeros, add_bias=True, **number_generation_kwargs)
     ann.train(training_examples, training_examples, 10000, 0.1)
 
     predictions = ann.feed_forward(training_examples)
